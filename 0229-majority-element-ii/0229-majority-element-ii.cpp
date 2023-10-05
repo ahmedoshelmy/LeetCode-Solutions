ec8f472b8067c44acf2cc3bd6f85e6bb86626703
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2023-10-05 09:11:37 +0300

    Time: 16 ms (7.44%), Space: 16.2 MB (28.20%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin() , nums.end()) ; 
         int n = nums.size() ;
-        vector<int> ans; 
+        unordered_map<int,int> cnt; 
         for(int i = 0 ; i<n;i++){
-            int j = i ; 
-            while(j <n && nums[i] == nums[j]) j++ ; 
-            if(j-i > n/3) ans.push_back(nums[i]); 
-            i = j-1; 
+            cnt[nums[i]]++; 
         }
-  
+        // O (n log n)
+        vector<int> ans; 
+        for(auto & [u,v]:cnt) if(v > n/3) ans.push_back(u);
         return ans; 
     }
 };
