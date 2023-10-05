c19e05a10bbdb0d139cd38b2fd391fed27e9fa21
Ahmed Osama Helmy <55411484+AhmedOsama198@users.noreply.github.com>
2023-10-05 09:05:57 +0300

    Time: 17 ms (7.44%), Space: 16.3 MB (9.38%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size() ;
+        map<int,int> cnt; 
+        for(int i = 0 ; i<n;i++){
+            cnt[nums[i]]++; 
+        }
+        // O (n log n)
+        vector<int> ans; 
+        for(auto & [u,v]:cnt) if(v > n/3) ans.push_back(u);
+        return ans; 
+    }
+};
